3a70d73ce865085374c38ba3cc611911e0db115a
onionys <onionys@gmail.com>
2015-08-05 23:24:15 +0000

    modified:   08-option-menu2.py

--- 01_ex/08-option-menu2.py
+++ 01_ex/08-option-menu2.py
@@ -42,6 +42,7 @@ class seg_line:
     def __init__(self, root,prog,seg , _row):
 
         self.frame = root
+        self.row = _row
 
         self.prog = prog
         self.seg = seg
@@ -60,6 +61,8 @@ class seg_line:
         self.seg_type_var = tk.StringVar(root)
         self.seg_type_var.set("%d" % self.seg_type)
         self.seg_type_entry = tk.Entry(self.frame, textvariable=self.seg_type_var)
+        self.seg_type_entry.bind('<Return>', self.seg_type_event)
+        self.seg_type_entry.bind('<FocusOut>', self.seg_type_event)
 
         self.seg_target_sp_var = tk.StringVar(self.frame)
         self.seg_target_sp_var.set("%f" % self.seg_target_sp) 
@@ -72,11 +75,12 @@ class seg_line:
         self.seg_call_prog_var = tk.StringVar(self.frame)
         self.seg_call_prog_var.set("%d" % self.seg_call_prog)
         self.seg_call_prog_entry = tk.Entry(self.frame, textvariable=self.seg_call_prog_var) 
+        self.seg_call_prog_entry.bind('<Return>',self.seg_call_prog_event)
+        self.seg_call_prog_entry.bind('<FocusOut>',self.seg_call_prog_event)
 
         self.seg_call_cycle_var = tk.StringVar(self.frame)
         self.seg_call_cycle_var.set("%d" % self.seg_call_cycle)
         self.seg_call_cycle_entry = tk.Entry(self.frame, textvariable=self.seg_call_cycle_var) 
-
 
         self.seg_target_pulse_var = tk.StringVar(self.frame)
         self.seg_target_pulse_var.set("%d" % self.seg_target_pulse)
@@ -85,10 +89,14 @@ class seg_line:
         self.seg_pulse_freq_var = tk.StringVar(self.frame)
         self.seg_pulse_freq_var.set("%d" % self.seg_pulse_freq)
         self.seg_pulse_freq_entry = tk.Entry(self.frame, textvariable=self.seg_pulse_freq_var)
+        self.seg_pulse_freq_entry.bind('<Return>',self.seg_pulse_freq_event)
+        self.seg_pulse_freq_entry.bind('<FocusOut>',self.seg_pulse_freq_event)
 
         self.seg_target_pos_var = tk.StringVar(self.frame)
         self.seg_target_pos_var.set("%s" % self.seg_target_pos)
         self.seg_target_pos_entry = tk.Entry(self.frame, textvariable=self.seg_target_pos_var)
+        self.seg_target_pos_entry.bind('<Return>',self.seg_target_pos_event)
+        self.seg_target_pos_entry.bind('<FocusOut>',self.seg_target_pos_event)
         #self.seg_time_var
 
         self.prog_label.grid(          row=_row,column=0)
@@ -102,12 +110,81 @@ class seg_line:
         self.seg_pulse_freq_entry.grid(row=_row,column=8)
         self.seg_target_pos_entry.grid(row=_row,column=9)
 
+        self.seg_type_event("")
+
+    def seg_type_event(self,event):
+        val = self.seg_type_var.get()
+        if(  val == "0"): ## 
+            self.seg_target_sp_entry.grid_forget()
+            self.seg_time_entry.grid_forget()
+            self.seg_call_prog_entry.grid_forget()
+            self.seg_call_cycle_entry.grid_forget()
+        elif(val == "1"):
+            self.seg_target_sp_entry.grid(row = self.row, column = 3)
+            self.seg_time_entry.grid(     row = self.row, column = 4)
+            self.seg_call_prog_entry.grid_forget()
+            self.seg_call_cycle_entry.grid_forget()
+        elif(val == "2"):
+            self.seg_target_sp_entry.grid(row = self.row, column = 3)
+            self.seg_time_entry.grid(     row = self.row, column = 4)
+            self.seg_call_prog_entry.grid_forget()
+            self.seg_call_cycle_entry.grid_forget()
+        elif(val == "3"):
+            self.seg_target_sp_entry.grid_forget() 
+            self.seg_time_entry.grid(     row = self.row, column = 4)
+            self.seg_call_prog_entry.grid_forget()
+            self.seg_call_cycle_entry.grid_forget()
+        elif(val == "4"):
+            self.seg_target_sp_entry.grid(row = self.row, column = 3)
+            self.seg_time_entry.grid_forget()
+            self.seg_call_prog_entry.grid_forget()
+            self.seg_call_cycle_entry.grid_forget()
+        elif(val == "5"):
+            self.seg_target_sp_entry.grid_forget()
+            self.seg_time_entry.grid_forget()
+            self.seg_call_prog_entry.grid(row = self.row, column = 5)
+            self.seg_call_cycle_entry.grid(row = self.row, column = 6)
+        else:
+            self.seg_type_var.set("0")
+
+    def seg_target_pos_event(self,event):
+        var = self.seg_target_pos_var.get()
+        if(not var in ['A','B','C','D']):
+            self.seg_target_pos_var.set("A")
+
+    def seg_pulse_freq_event(self,event):
+        var = self.seg_pulse_freq_var.get()
+        try:
+            var = int(var)
+            if( not ( (var >= 1) and (var <= 30.0) )):
+                self.seg_pulse_freq_var.set("1")
+        except:
+            self.seg_pulse_freq_var.set("1")
+
+
+    def seg_call_prog_event(self,event):
+        var = self.seg_call_prog_var.get()
+        try:
+            var = int(var)
+            if(self.prog == 20):
+                self.seg_call_prog_var.set("None")
+            elif( not ((var > self.prog)and(var <= 20))):
+                self.seg_call_prog_var.set(str(self.prog + 1))
+        except:
+            if(self.prog == 20):
+                self.seg_call_prog_var.set("None")
+            else:
+                self.seg_call_prog_var.set(str(self.prog + 1))
+            
+
 
 
 
 class seg_editor:
 
     def __init__(self,prog):
+        if not prog in range(21):
+            prog = 0
         self.root = tk.Tk()
         self.seg_h = seg_head(self.root)
         self.segs = []
@@ -140,7 +217,6 @@ class seg_editor:
 
 
 
-
 if __name__ == "__main__":
-    seditor = seg_editor(10)
+    seditor = seg_editor(11)
     seditor.root.mainloop()
