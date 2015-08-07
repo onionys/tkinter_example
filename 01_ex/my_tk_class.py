c1258fe0c11478b0255409eddadc52e1d494e4b2
onionys <onionys@gmail.com>
2015-08-07 17:01:17 +0800

    modified:   my_tk_class.py

--- 01_ex/my_tk_class.py
+++ 01_ex/my_tk_class.py
@@ -1,15 +1,26 @@
 #!/usr/bin/env python3
 
 import tkinter as tk
+from PIL import ImageTk, Image
 
-class swbtn_img(tk.Button):
-    def __init__(self,img_on,img_off):
-        tk.Button
+class sw_btn_img(tk.Button):
+
+    def __init__(self,root,img_on,img_off):
+        tk.Button.__init__(self=self,master=root,text="",command=self.command,image=img_off)
         self.img_on  = img_on
         self.img_off = img_off
-        self.
+        self.condition = False
 
-
+    def command(self):
+        self.condition = False if self.condition else True
+        self.config(image=self.img_on if self.condition else self.img_off)
 
 if __name__=="__main__":
-    print ("test:w")
+    root = tk.Tk()
+    im_on = Image.open('./pic/btn_sw_on.gif')
+    img_on = ImageTk.PhotoImage(im_on)
+    im_off = Image.open('./pic/btn_sw_off.gif')
+    img_off = ImageTk.PhotoImage(im_off)
+    btn = sw_btn_img(root,img_on, img_off)
+    btn.pack()
+    root.mainloop()
